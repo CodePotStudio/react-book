3503d94b7ea0689bfab0c40203123c4a320550d0
toycrane <alwaysfun2183@gmail.com>
2021-02-09 00:52:12 +0900

    리액트 프로젝트 시작하기 페이지 작성

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
+	someSidebar: {
+		"간단한 퀴즈 앱 만들기 (1주차)": ["week01/doc1", "doc2", "doc3"],
+		"퀴즈 앱 배포하기 (2주차)": ["mdx"],
+	},
 };

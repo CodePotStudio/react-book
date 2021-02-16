0d34e68224f8ffad168ac9cb1b5b4c0294c835d7
toycrane <alwaysfun2183@gmail.com>
2021-02-16 16:05:02 +0900

    1주차 수업 자료 업로드

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,15 @@
 module.exports = {
 	someSidebar: {
-		"간단한 퀴즈 앱 만들기 (1주차)": ["week01/doc1", "week01/doc2"],
+		"간단한 퀴즈 앱 만들기 (1주차)": [
+			"week01/doc1",
+			"week01/doc2",
+			"week01/doc3",
+			"week01/doc4",
+			"week01/doc5",
+			"week01/doc6",
+			"week01/doc7",
+			"week01/doc8",
+		],
 		"퀴즈 앱 배포하기 (2주차)": ["mdx"],
 	},
 };

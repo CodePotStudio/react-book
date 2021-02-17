b0d9b8c326207cf2f8511cfbc06a233c3da5f5c4
toycrane <alwaysfun2183@gmail.com>
2021-02-17 11:55:39 +0900

    설정 파일 변경

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,15 +1,15 @@
 module.exports = {
 	title: "{React 개발에서 배포까지}",
 	tagline: "8주 만에 나만의 성향 테스트 만들기",
-	url: "https://your-docusaurus-test-site.com",
+	url: "https://react.codepot.kr",
 	baseUrl: "/",
 	onBrokenLinks: "throw",
 	onBrokenMarkdownLinks: "warn",
 	favicon: "img/favicon.ico",
-	url: "https://code-pot-react.netlify.com", // Url to your site with no trailing slash
+	url: "https://react.codepot.kr", // Url to your site with no trailing slash
 	baseUrl: "/",
-	projectName: "CodePotStudio.github.io",
-	organizationName: "CodePotStudio",
+	projectName: "Code Pot React Class",
+	organizationName: "CodePot Company",
 	projectName: "react-book", // Usually your repo name.
 	themeConfig: {
 		navbar: {
@@ -23,11 +23,6 @@ module.exports = {
 					activeBasePath: "docs",
 					label: "수업자료",
 					position: "left",
-				},
-				{
-					href: "https://github.com/CodePotStudio/React-Class-001",
-					label: "GitHub",
-					position: "right",
 				},
 			],
 		},
@@ -43,14 +38,7 @@ module.exports = {
 				docs: {
 					sidebarPath: require.resolve("./sidebars.js"),
 					// Please change this to your repo.
-					editUrl:
-						"https://github.com/facebook/docusaurus/edit/master/website/",
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					editUrl:
-						"https://github.com/facebook/docusaurus/edit/master/website/blog/",
+					editUrl: "https://github.com/CodePotStudio/react-book/edit/master/",
 				},
 				theme: {
 					customCss: require.resolve("./src/css/custom.css"),
